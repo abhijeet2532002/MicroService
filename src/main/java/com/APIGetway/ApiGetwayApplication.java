02ff4cc677acba7882f2efa7f97232c833e379ed
abhij <abhijeetkumar2532002@gmail.com>
2023-02-16 20:58:48 +0530

    Initial commit

---
+++ src/main/java/com/APIGetway/ApiGetwayApplication.java
@@ -0,0 +1,14 @@
+package com.APIGetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetwayApplication.class, args);
+		System.out.println("\n\t\t..... API gateway service in microservices .....\n");
+	}
+
+}
